16f12a9097fef898d2bde9f8577c2580f9782d76
Morton Jonuschat <yabawock@gmail.com>
2014-10-16 21:22:43 +0200

    Track execution statistics of all SQL statements executed by server

--- my_init.d/02-set-database-password.sh
+++ my_init.d/02-set-database-password.sh
@@ -10,4 +10,5 @@ fi
 # run idemptoent database configuration
 sudo -i -u postgres /usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config-file=${PG_CONFIG} <<EOF
 ALTER ROLE postgres WITH PASSWORD '${DB_ADMIN_PASS}';
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 EOF
